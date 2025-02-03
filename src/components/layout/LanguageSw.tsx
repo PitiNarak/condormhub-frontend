56f9230b18ab27e6443b5f1d6624fae7acd39472
TokiDOkiiiiiiii <napatphong46@gmail.com>
2025-02-03 10:54:55 +0700

    feat: add LanguageSw

---
+++ src/components/layout/LanguageSw.tsx
@@ -0,0 +1,34 @@
+'use client';
+import React from 'react';
+
+//It is for the tailwind to work with the dynamic classname
+const bgc1 = 'bg-white';
+const bgc2 = 'bg-black';
+const txtc1 = 'text-white';
+const txtc2 = 'text-black';
+const bold = 'font-bold';
+
+export default function LanguageSw() {
+  const [EN_TH, setEN_TH] = React.useState(true);
+  const toggle = () => {
+    setEN_TH(!EN_TH);
+    console.log(EN_TH);
+  };
+  return (
+    <div className="flex-1 border-red-700 border-">
+      <button
+        className={`${!EN_TH ? bgc1 : bgc2} ${EN_TH ? txtc1 : txtc2} disabled: px-5 py-3 rounded-l ${!EN_TH ? bold : ''}`}
+        onClick={toggle}
+      >
+        TH
+      </button>
+
+      <button
+        className={`${EN_TH ? bgc1 : bgc2} ${!EN_TH ? txtc1 : txtc2} px-5 py-3 rounded-r ${EN_TH ? bold : ''}`}
+        onClick={toggle}
+      >
+        EN
+      </button>
+    </div>
+  );
+}
