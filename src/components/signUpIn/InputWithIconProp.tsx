be56498df69b39de45d74fa099d2550356be8ec4
TokiDOkiiiiiiii <napatphong46@gmail.com>
2025-02-07 15:02:02 +0700

    fix: input checker bug

--- src/components/signUpIn/InputWithIconProp.tsx
+++ src/components/signUpIn/InputWithIconProp.tsx
@@ -2,30 +2,28 @@ import { Input } from '../ui/input';
 
 interface InputWithIconProp {
   icon: React.ReactNode;
-  label?: string;
-  name: string;
   placeholder: string;
   type: string;
+  fields: React.HtmlHTMLAttributes<HTMLInputElement>;
 }
 
 const InputWithIcon = ({
   icon,
-  label,
-  name,
   placeholder,
   type,
+  fields,
 }: InputWithIconProp) => {
   return (
-    <div className="">
-      <label htmlFor={name}>{label}</label>
+    <div>
       <div className="relative">
-        <div className="absolute left-1 top-1 h-4 w-4 text-muted-foreground">
+        <div className="absolute left-[6px] top-[6px] h-4 w-4 text-muted-foreground">
           {icon}
         </div>
         <Input
           type={type}
           placeholder={placeholder}
-          className="w-full rounded-lg bg-background pl-10"
+          className="w-full rounded-lg bg-background pl-9"
+          {...fields}
         />
       </div>
     </div>
