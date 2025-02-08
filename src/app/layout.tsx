70a7c4b0e0cfd0b2070aefc5cd98034509a97244
Thanapon Johdee <sern.dev@gmail.com>
2025-02-08 17:17:23 +0700

    fix: add correct layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import '@/app/globals.css';
-import Header from '@/components/layout/Header';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -28,10 +27,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <div className="h-screen">
-          <Header />
-          {children}
-        </div>
+        {children}
       </body>
     </html>
   );
