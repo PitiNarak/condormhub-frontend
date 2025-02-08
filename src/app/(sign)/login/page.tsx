00458759c546764c90e0e9933a421e55803c26ed
Naron <naron834@gmail.com>
2025-02-08 18:37:17 +0700

    feat: add session check

--- src/app/(sign)/login/page.tsx
+++ src/app/(sign)/login/page.tsx
@@ -6,28 +6,35 @@ import {
   CardHeader,
   CardTitle,
 } from '@/components/ui/card';
+import { auth } from '@/lib/auth';
 import { cn } from '@/lib/utils';
+import { redirect } from 'next/navigation';
 
-export default function Page({
+export default async function Page({
   className,
 }: React.ComponentPropsWithoutRef<'div'>) {
-  return (
-    <div className="flex w-full items-center justify-center h-[100vh]">
-      <div className="w-full max-w-sm">
-        <div className={cn('flex flex-col gap-6', className)}>
-          <Card>
-            <CardHeader>
-              <CardTitle className="text-2xl">Login</CardTitle>
-              <CardDescription>
-                Enter your email below to login to your account
-              </CardDescription>
-            </CardHeader>
-            <CardContent>
-              <LoginForm />
-            </CardContent>
-          </Card>
+  const session = await auth();
+  if (session) {
+    redirect('/home');
+  } else {
+    return (
+      <div className="flex w-full items-center justify-center h-[100vh]">
+        <div className="w-full max-w-sm">
+          <div className={cn('flex flex-col gap-6', className)}>
+            <Card>
+              <CardHeader>
+                <CardTitle className="text-2xl">Login</CardTitle>
+                <CardDescription>
+                  Enter your email below to login to your account
+                </CardDescription>
+              </CardHeader>
+              <CardContent>
+                <LoginForm />
+              </CardContent>
+            </Card>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
