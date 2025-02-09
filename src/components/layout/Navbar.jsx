55501757d1c7bd0dfaa46b861921de752754a861
TokiDOkiiiiiiii <napatphong46@gmail.com>
2025-02-09 15:31:36 +0700

    feat: add navbar

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,9 +1,44 @@
-const Navbar = () => {
+import LanguageSw from '@/components/layout/LanguageSw';
+import Link from 'next/link';
+import { Bell, Search } from 'lucide-react';
+
+const Header = () => {
   return (
-    <div>
-      <p className="text-xl">This is Navbar</p>
+    <div className="flex shadow-md items-center h-20">
+      <div className="text-center flex-row px-[2%]">
+        <div className="flex-1">
+          <Link href="/">
+            <p className="text-xl font-extrabold">ConDormHub</p>
+          </Link>
+        </div>
+        <div className="flex-1">
+          <LanguageSw />
+        </div>
+      </div>
+      <div className="flex-[4] flex">
+        <div className="flex-1 text-sm xl:text-base pr-5">
+          <Link href="#">Chat</Link>
+        </div>
+        <div className="flex-1 text-sm xl:text-base">
+          <Link href="#">Setting</Link>
+        </div>
+        <div className="flex-[5] flex text-center">
+          <div className="flex-[1]"></div>
+          <div className="flex-1 justify-end m-auto flex">
+            <div className="">
+              <Bell />
+            </div>
+          </div>
+          <div className="flex-1 text-sm xl:text-base">
+            <Link href="#">Profile</Link>
+          </div>
+          <div className="pr-[2%]">
+            <Search />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
 
-export default Navbar;
+export default Header;
