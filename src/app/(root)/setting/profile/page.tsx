7dc855ed2c05aba7df4e6648bfd97836ba424436
Naron <naron834@gmail.com>
2025-02-09 22:11:47 +0700

    style: add divider

--- src/app/(root)/setting/profile/page.tsx
+++ src/app/(root)/setting/profile/page.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import ChangeEmailDialog from '@/components/profileSetting/ChangeEmailDialog';
 import UpdateInformationForm from '@/components/profileSetting/UpdateInformationForm';
 import { redirect } from 'next/navigation';
+import Divider from '@/components/layout/Divider';
 
 const page = async () => {
   const session = await auth();
@@ -11,8 +12,9 @@ const page = async () => {
     return (
       <div className="flex flex-col justify-center items-center p-10 gap-6">
         <div className="flex max-w-3xl w-full">
-          <h1 className="text-3xl py-3 font-semibold">Profile Settings</h1>
+          <h1 className="text-3xl pt-3 font-semibold">Profile Settings</h1>
         </div>
+        <Divider className="max-w-3xl w-full" />
         <div className="flex flex-col min-w-96 gap-y-1 w-full max-w-3xl">
           <h2 className="font-semibold">Email</h2>
           <div className="flex items-center gap-10 w-full">
