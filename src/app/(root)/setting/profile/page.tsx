975109922c05be9f51c5496627d26c8c7fe01a42
Naron <naron834@gmail.com>
2025-02-10 00:20:09 +0700

    feat: add email verificaiton status

--- src/app/(root)/setting/profile/page.tsx
+++ src/app/(root)/setting/profile/page.tsx
@@ -1,9 +1,9 @@
 import { auth } from '@/lib/auth';
-import React from 'react';
 import ChangeEmailDialog from '@/components/profileSetting/ChangeEmailDialog';
 import UpdateInformationForm from '@/components/profileSetting/UpdateInformationForm';
 import { redirect } from 'next/navigation';
 import Divider from '@/components/layout/Divider';
+import VerificationStatus from '@/components/profileSetting/VerificationStatus';
 
 const page = async () => {
   const session = await auth();
@@ -16,7 +16,10 @@ const page = async () => {
         </div>
         <Divider className="max-w-3xl w-full" />
         <div className="flex flex-col min-w-96 gap-y-1 w-full max-w-3xl">
-          <h2 className="font-semibold">Email</h2>
+          <div className="flex gap-3">
+            <h2 className="font-semibold">Email</h2>
+            <VerificationStatus />
+          </div>
           <div className="flex items-center gap-10 w-full">
             <p>{session?.user.email}</p>
             <ChangeEmailDialog />
