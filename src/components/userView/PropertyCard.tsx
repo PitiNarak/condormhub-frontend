dacad1206d06cd77295fd39dd9279c02e0695747
TokiDOkiiiiiiii <napatphong46@gmail.com>
2025-02-10 12:34:19 +0700

    style: change property-scroll and property card to ABNB like page

--- src/components/userView/PropertyCard.tsx
+++ src/components/userView/PropertyCard.tsx
@@ -1,38 +1,39 @@
 import * as React from 'react';
-
+import Link from 'next/link';
 import { PropertyDetailButton } from './PropertyDetail';
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from '@/components/ui/card';
+import { Card, CardContent } from '@/components/ui/card';
 import Image from 'next/image';
 
 export default function PropertyCard() {
   return (
-    <Card className="w-full">
-      <CardHeader>
-        <CardTitle className="text-xl">CondoName</CardTitle>
-        <CardDescription>
-          Clickable Seller&apos;s Profile and Rating
-        </CardDescription>
-      </CardHeader>
-      <CardContent className="pb-3">
-        <div className="flex">
-          <div className="justify-center m-auto">
-            <Image src="/image.jpeg" width={300} height={200} alt="Img"></Image>
+    <Card className="w-full shadow-none border-none">
+      <CardContent className="pb-3 px-0">
+        <div className="">
+          <div className="justify-center mx-auto w-full h-full mb-2 relative">
+            <Image
+              src="/image.jpeg"
+              alt="Description of image"
+              width={500}
+              height={300}
+              className="object-cover rounded-md"
+            />
+            <Link
+              href="/navigation/profile"
+              className="absolute top-2 right-1 bg-white px-3 py-1 rounded-2xl"
+            >
+              Piti <span>4.9 ⭐</span>
+            </Link>
           </div>
-          <div className="flex-1 ml-11 text-start hidden md:block">
-            <p>2 Bed 2 Bath</p>
-            <p>32 sq.m.</p>
-            <p>254 Phaya Thai Rd, Wang Mai, Pathum Wan, Bangkok 10330</p>
-            <p>15,000 Baht</p>
+          <div className="flex">
+            <div className="pl-2">
+              <p className="font-bold">Pathum Wan, Bangkok</p>
+              <p>2 Bed 2 Bath</p>
+              <p>15,000 Baht</p>
+            </div>
+            <div className="justify-end mx-auto content-center">
+              <PropertyDetailButton />
+            </div>
           </div>
-        </div>
-        <div className="mt-2 text-center md:text-end">
-          <PropertyDetailButton />
         </div>
       </CardContent>
     </Card>
--- src/components/userView/PropertyScroll.tsx
+++ src/components/userView/PropertyScroll.tsx
@@ -1,27 +1,23 @@
-import * as React from 'react';
-
-import { ScrollArea } from '@/components/ui/scroll-area';
-import { Separator } from '@/components/ui/separator';
-import PropertyCard from '@/components/userView/PropertyCard';
-
+import PropertyCard from './PropertyCard';
 const tags = Array.from({ length: 50 }).map(
   (_, i, a) => `v1.2.0-beta.${a.length - i}`
 );
 
 export function PropertyScroll() {
   return (
-    <ScrollArea className="h-[600px] w-9/12 max-w-[800px] rounded-md border items-center m-auto">
-      <div className="p-4">
-        <h4 className="mb-4 text-xl font-medium leading-none">No Filters</h4>
+    <div className="shadow-md border border-gray-100 pt-14 pb-12">
+      <p className="block text-center text-lg w-[150px] mb-8 ml-2 rounded-2xl border border-gray-400">
+        No Filters
+      </p>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 mx-5">
         {tags.map((tag) => (
           <>
             <div key={tag} className="text-sm">
               <PropertyCard />
             </div>
-            <Separator className="my-4" />
           </>
         ))}
       </div>
-    </ScrollArea>
+    </div>
   );
 }
