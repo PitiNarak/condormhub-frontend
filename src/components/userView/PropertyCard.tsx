04c4ff33153011144f7fb132f39ecb515b22a9d1
TokiDOkiiiiiiii <napatphong46@gmail.com>
2025-02-11 18:28:23 +0700

    refactor: remove name and change star position

--- src/components/userView/PropertyCard.tsx
+++ src/components/userView/PropertyCard.tsx
@@ -1,8 +1,8 @@
 import * as React from 'react';
-import Link from 'next/link';
 import { PropertyDetailButton } from './PropertyDetail';
 import { Card, CardContent } from '@/components/ui/card';
 import Image from 'next/image';
+import { Star } from 'lucide-react';
 interface PropertyProp {
   image: string;
   owner: string;
@@ -24,6 +24,7 @@ export default function PropertyCard({
   district,
   price,
 }: PropertyProp) {
+  console.log(owner);
   return (
     <Card className="w-full shadow-none border-none">
       <CardContent className="pb-3 px-0">
@@ -36,12 +37,6 @@ export default function PropertyCard({
               height={300}
               className="object-cover rounded-md"
             />
-            <Link
-              href="/navigation/profile"
-              className="absolute top-2 right-1 bg-white px-3 py-1 rounded-2xl"
-            >
-              {owner} <span>{rating} ⭐</span>
-            </Link>
           </div>
           <div className="flex">
             <div className="pl-2">
@@ -54,6 +49,10 @@ export default function PropertyCard({
               <p>{price} Baht</p>
             </div>
             <div className="flex-1 relative">
+              <div className="absolute top-0 right-0 flex ">
+                <p className="content-center">{rating}&nbsp;</p>
+                <Star size={18} />
+              </div>
               <div className="absolute bottom-0 right-0">
                 <PropertyDetailButton />
               </div>
