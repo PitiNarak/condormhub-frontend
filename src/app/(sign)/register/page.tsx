cd0e304c687315b36495b75231d2d9d1815a9471
TokiDOkiiiiiiii <napatphong46@gmail.com>
2025-02-12 13:56:39 +0700

    feat: add session checker on (sign)

--- src/app/(sign)/register/page.tsx
+++ src/app/(sign)/register/page.tsx
@@ -1,18 +1,11 @@
 import { RegisterBox } from '@/components/signUpIn/RegisterBox';
-import { auth } from '@/lib/auth';
-import { redirect } from 'next/navigation';
 
-export default async function Page() {
-  const session = await auth();
-  if (session) {
-    redirect('/home');
-  } else {
-    return (
-      <div className="justify-center m-auto pt-14 flex w-9/12 max-w-[450px]">
-        <div className="flex-1">
-          <RegisterBox />
-        </div>
+export default function Page() {
+  return (
+    <div className="justify-center m-auto pt-14 flex w-9/12 max-w-[450px]">
+      <div className="flex-1">
+        <RegisterBox />
       </div>
-    );
-  }
+    </div>
+  );
 }
