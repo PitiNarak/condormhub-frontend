5bdd17bd1ecfe7248091d57aaf1069139f4ff625
Naron <naron834@gmail.com>
2025-02-16 18:16:34 +0700

    refactor: add refresh token and token expire mock

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -13,31 +13,35 @@ declare module 'next-auth' {
       id: string;
       email: string;
     };
-    accessToken?: string;
+    access_token?: string;
+    refresh_token?: string;
+    access_token_expired?: number;
   }
   interface User {
-    accessToken: string;
+    access_token: string;
+    refresh_token?: string;
+    access_token_expired?: number;
   }
 }
 
 declare module 'next-auth/jwt' {
   interface JWT extends DefaultJWT {
     id: string;
-    accessToken?: string;
-    // refresh_token?: string;
-    // access_token_expired?: number;
+    access_token?: string;
+    refresh_token?: string;
+    access_token_expired?: number;
     user: User;
   }
 }
 
 // Simulate an actual token refresh request
-// async function refreshToken() {
-//   return {
-//     access_token: 'NEW_ACCESS_TOKEN',
-//     refresh_token: 'NEW_REFRESH_TOKEN',
-//     access_token_expired: Date.now() + 3600 * 1000,
-//   };
-// }
+async function refreshToken() {
+  return {
+    access_token: 'NEW_ACCESS_TOKEN',
+    refresh_token: 'NEW_REFRESH_TOKEN',
+    access_token_expired: Date.now() + 3600 * 1000,
+  };
+}
 
 export const nextAuthConfig = {
   providers: [
@@ -72,7 +76,9 @@ export const nextAuthConfig = {
             return {
               id: data.userInformation.id,
               email: credentials.email || '',
-              accessToken: data.accessToken,
+              access_token: data.accessToken,
+              refresh_token: 'NEW_REFRESH_TOKEN',
+              access_token_expired: Date.now() + 3600 * 1000,
             };
           } else {
             console.log(response.status);
@@ -90,27 +96,28 @@ export const nextAuthConfig = {
     jwt: async ({ token, user }) => {
       if (user) {
         token.user = user;
-        token.accessToken = user.accessToken;
+        token.access_token = user.access_token;
+        token.refresh_token = user.refresh_token;
+        token.access_token_expired = user.access_token_expired;
       }
 
-      // if (
-      //   token.access_token_expired &&
-      //   token.access_token_expired < Date.now()
-      // ) {
-      //   const refreshed = await refreshToken();
+      if (
+        token.access_token_expired &&
+        token.access_token_expired < Date.now()
+      ) {
+        const refreshed = await refreshToken();
 
-      //   token.access_token = refreshed.access_token;
-      //   token.refresh_token = refreshed.refresh_token;
-      //   token.access_token_expired = refreshed.access_token_expired;
-      //   token.user.access_token = refreshed.access_token;
-      //   token.user.refresh_token = refreshed.refresh_token;
-      //   token.user.access_token_expired = refreshed.access_token_expired;
-      // }
+        token.access_token = refreshed.access_token;
+        token.refresh_token = refreshed.refresh_token;
+        token.access_token_expired = refreshed.access_token_expired;
+      }
 
       return token;
     },
     session: async ({ session, token }) => {
-      session.accessToken = token.accessToken;
+      session.access_token = token.access_token;
+      session.refresh_token = token.refresh_token;
+      session.access_token_expired = token.access_token_expired;
       session.user.id = token.user.id;
       return session;
     },
