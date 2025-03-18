e2f2a54974371ef3b5c5c3c6213cb5106ab0d03c
TokiDOkiiiiiiii <napatphong46@gmail.com>
2025-03-18 13:05:15 +0700

    fix: fix display function in display.ts

--- src/function/display.ts
+++ src/function/display.ts
@@ -6,7 +6,7 @@ export function displayPrice(price: number) {
   let result = '';
   while (price >= 1000) {
     result = pad(String(price % 1000)) + ',' + result;
-    price /= 1000;
+    price = Math.floor(price / 1000);
   }
   result = String(price % 1000) + ',' + result;
   return result.slice(0, result.length - 1);
