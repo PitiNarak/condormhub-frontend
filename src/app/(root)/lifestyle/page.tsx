0054537f64df02c5f27d023e5c4df9d63d66b7b0
Subhakrit Chokchaitam <keenchokchaitam@gmail.com>
2025-03-20 09:20:20 +0700

    refactor: change style of lifestyle tag page (not use anymore))

--- src/app/(root)/lifestyle/page.tsx
+++ src/app/(root)/lifestyle/page.tsx
@@ -1,12 +1,14 @@
 import { AuthProvider } from '@/components/auth/authProvider';
 import { LifestyleForm } from '@/components/lifestyle-page/lifestyleForm';
+import Divider from '@/components/layout/Divider';
 
 export default function LifestyleTagSelectorPage() {
   return (
-    <div className="bg-gray-100 min-h-screen">
-      <div className="flex flex-col items-center py-10 px-4">
-        <p className="text-4xl font-bold text-gray-800">Lifestyle Collection</p>
-        <p className="text-gray-600 mt-2">
+    <div className="flex flex-col justify-center items-center p-10 gap-6min-h-screen">
+      <div className="flex flex-col gap-3 max-w-3xl w-full">
+        <h1 className="text-3xl pt-3 font-semibold">Lifestyle Collection</h1>
+        <Divider className="max-w-3xl w-full" />
+        <p className="text-gray-700">
           Please choose your lifestyle from this collection
         </p>
       </div>
