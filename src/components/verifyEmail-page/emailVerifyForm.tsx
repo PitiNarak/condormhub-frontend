cbd9e7814bec3fe7bdcbc64b85d2456a45f0e4b7
Thanapon Johdee <sern.dev@gmail.com>
2025-03-21 15:08:16 +0700

    feat: implement verify action

--- src/components/verifyEmail-page/emailVerifyForm.tsx
+++ src/components/verifyEmail-page/emailVerifyForm.tsx
@@ -1,32 +1,25 @@
 'use client';
 
-// import { verifyEmail } from '@/actions/verifyEmail/verify';
-// import {
-//   Card,
-//   CardContent,
-//   CardDescription,
-//   CardFooter,
-//   CardHeader,
-//   CardTitle,
-// } from '@/components/ui/card';
+import { verifyEmail } from '@/actions/verifyEmail/verify';
 import { EmailVerifyCard } from '@/components/verifyEmail-page/emailVerifyCard';
-// import { useSearchParams } from 'next/navigation';
+import { redirect, useSearchParams } from 'next/navigation';
+import { useEffect } from 'react';
 
 export function EmailVerifyForm() {
-  // const searchParams = useSearchParams();
-  // const token = searchParams.get('token');
-  // if (!token) {
-  //   redirect('/');
-  // }
-  // useEffect(() => {
-  //   const res = verifyEmail(token ?? '');
-  //   res
-  //     .then((data) => {
-  //       console.log(data);
-  //     })
-  //     .catch((error) => {
-  //       console.error(error);
-  //     });
-  // }, []);
+  const searchParams = useSearchParams();
+  const token = searchParams.get('token');
+  if (!token) {
+    redirect('/');
+  }
+  useEffect(() => {
+    const res = verifyEmail(token ?? '');
+    res
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
   return <EmailVerifyCard isLoading={false} error="" />;
 }
