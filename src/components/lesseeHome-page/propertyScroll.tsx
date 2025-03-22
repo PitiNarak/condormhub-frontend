57a9a1a31ee542353434d2e322a3eeca1140bf27
Subhakrit Chokchaitam <keenchokchaitam@gmail.com>
2025-03-22 16:05:25 +0700

    refactor: add responsive to lessee view page

--- src/components/lesseeHome-page/propertyScroll.tsx
+++ src/components/lesseeHome-page/propertyScroll.tsx
@@ -25,11 +25,11 @@ export async function PropertyScroll({ page }: PropertyScrollProps) {
   }
 
   return (
-    <div className="shadow-md border border-gray-100 pt-14 pb-12">
+    <div className="shadow-md border border-gray-100 pt-14 pb-12 overflow-hidden">
       <p className="block text-center text-lg w-[150px] mb-8 ml-2 rounded-2xl border border-gray-400">
         No Filters
       </p>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 mx-5">
+      <div className="flex flex-wrap justify-center gap-3 mx-5">
         {propertyData.map(
           (data: components['schemas']['dto.DormResponseBody']) => (
             <div key={String(data.id)} className="text-sm">
