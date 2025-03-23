b07fb4a8153c44bc39a84744d773dafe5532c6dc
TokiDOkiiiiiiii <napatphong46@gmail.com>
2025-03-23 21:56:52 +0700

    feat: format date before send to backend

--- src/actions/personalInfo/sendPersonalInfo.ts
+++ src/actions/personalInfo/sendPersonalInfo.ts
@@ -7,12 +7,16 @@ export async function sendPersonalInfo(
   access_token: string,
   formData: components['schemas']['dto.UserInformationRequestBody']
 ) {
+  const date: Date = new Date(formData.birthDate ?? '2000-01-01');
+  formData['birthDate'] = date.toISOString();
+  console.log(formData);
   const { data, error } = await client.PATCH('/user', {
     body: formData,
     headers: {
       Authorization: `Bearer ${access_token}`,
     },
   });
+  console.log(data);
   if (error || !data.data) {
     return {
       message: error?.error,
