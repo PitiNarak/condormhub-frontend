ceabe983bdd0464d8a0256b4015acd14ce07aa7f
Subhakrit Chokchaitam <keenchokchaitam@gmail.com>
2025-03-24 15:39:42 +0700

    feat: implement upload student images api

---
+++ src/actions/studentVerification/uploadStudentImage.ts
@@ -0,0 +1,34 @@
+'use server';
+
+import { revalidateTag } from 'next/cache';
+
+export async function uploadStudentImage(file: File, access_token: string) {
+  const formData = new FormData();
+
+  formData.append('image', file);
+
+  try {
+    const response = await fetch(
+      `${process.env.BACKEND_URL}/user/studentEvidence`,
+      {
+        method: 'POST',
+        body: formData,
+        headers: {
+          Authorization: `Bearer ${access_token}`,
+        },
+      }
+    );
+
+    const result = await response.json();
+
+    if (response.ok) {
+      revalidateTag('student-evidence');
+      return result.data; // { expired: 'string', url: 'string' }
+    } else {
+      return { error: result.error || 'An unknown error occurred.' };
+    }
+  } catch (error) {
+    console.error('Unexpected error:', error);
+    return { error: 'An unexpected error occurred.' };
+  }
+}
