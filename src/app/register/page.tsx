e99843f00b431ad6549f01c5698a06ee81577c27
TokiDOkiiiiiiii <napatphong46@gmail.com>
2025-03-26 00:54:40 +0700

    fix: remove session checker so that it will not redirect when the placeholder session is created

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,13 +1,6 @@
 import { RegisterBox } from '@/components/register-page/registerBox';
-import { auth } from '@/lib/auth';
-import { redirect } from 'next/navigation';
 
 export default async function Page() {
-  const session = await auth();
-  if (session) {
-    redirect('/');
-  }
-
   return (
     <div className="flex items-center justify-center">
       <div className="flex-1 w-full max-w-md pt-14 mx-4">
