437e7b5ed223cc566eb9580745920cd73292ac22
Thanapon Johdee <sern.dev@gmail.com>
2025-03-27 03:15:07 +0700

    feat: leasing request action

---
+++ src/actions/leasingRequest/cancel.ts
@@ -0,0 +1,37 @@
+'use server';
+
+import client from '@/api';
+import { auth } from '@/lib/auth';
+
+export async function approve(id: string) {
+  try {
+    const session = await auth();
+    const access_token = session?.access_token;
+    if (!access_token) {
+      throw new Error('no session');
+    }
+
+    const { data, error } = await client.PATCH('/request/{id}/cancel', {
+      params: { path: { id } },
+      headers: {
+        Authorization: `Bearer ${access_token}`,
+      },
+    });
+
+    if (error) {
+      throw new Error(error.error);
+    }
+
+    return { data };
+  } catch (e: unknown) {
+    console.error(e);
+    if (e instanceof Error) {
+      return {
+        error: e.message,
+      };
+    }
+    return {
+      error: 'unknown error',
+    };
+  }
+}
---
+++ src/actions/leasingRequest/getMe.ts
@@ -0,0 +1,36 @@
+'use server';
+
+import client from '@/api';
+import { auth } from '@/lib/auth';
+
+export async function getMyLeasingRequest(limit = 50, page = 1) {
+  try {
+    const session = await auth();
+    const access_token = session?.access_token;
+    if (!access_token) {
+      throw new Error('no session');
+    }
+
+    const { data, error } = await client.GET('/request/me', {
+      params: { query: { limit, page } },
+      headers: {
+        Authorization: `Bearer ${access_token}`,
+      },
+    });
+
+    if (error) {
+      throw new Error(error.error);
+    }
+    return { data };
+  } catch (e: unknown) {
+    console.error(e);
+    if (e instanceof Error) {
+      return {
+        error: e.message,
+      };
+    }
+    return {
+      error: 'unknown error',
+    };
+  }
+}
