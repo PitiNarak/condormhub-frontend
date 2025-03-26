1a15590b6caedad003efb42de2b9ee1970511e12
TokiDOkiiiiiiii <napatphong46@gmail.com>
2025-03-26 18:20:52 +0700

    feat: send to /personalInfo after got session

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,6 +1,13 @@
 import { RegisterBox } from '@/components/register-page/registerBox';
+import { auth } from '@/lib/auth';
+import { redirect } from 'next/navigation';
 
 export default async function Page() {
+  const session = await auth();
+  if (session) {
+    redirect('/personalInfo');
+  }
+
   return (
     <div className="flex items-center justify-center">
       <div className="flex-1 w-full max-w-md pt-14 mx-4">
