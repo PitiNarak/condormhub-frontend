3e00e7960cf4126ab5a908a0d876819568b1a6a0
Subhakrit Chokchaitam <keenchokchaitam@gmail.com>
2025-03-28 00:01:06 +0700

    feat: calling api to fetch property by owner id

---
+++ src/actions/lessorDashboard/fetchMyProperty.ts
@@ -0,0 +1,54 @@
+'use server';
+
+import client from '@/api';
+
+export async function fetchOwnerProperty(
+  OwnerID: string,
+  page: number = 1,
+  limit: number = 12
+) {
+  if (!OwnerID) {
+    return {
+      message: 'Owner ID is required.',
+      pagination: {
+        current_page: 1,
+        last_page: 1,
+        limit: 12,
+        total: 0,
+      },
+    };
+  }
+
+  try {
+    // Make API call to fetch properties by owner ID
+    const { data, error } = await client.GET('/dorms/owner/{id}', {
+      params: {
+        path: { id: OwnerID },
+        query: { page, limit },
+      },
+    });
+
+    if (error || !data.data) {
+      return {
+        message: error?.error || 'An error occurred.',
+        pagination: {
+          current_page: 1,
+          last_page: 1,
+          limit: 12,
+          total: 0,
+        },
+      };
+    }
+    return data;
+  } catch (err) {
+    return {
+      message: `Unexpected error occurred. ${err}`,
+      pagination: {
+        current_page: 1,
+        last_page: 1,
+        limit: 12,
+        total: 0,
+      },
+    };
+  }
+}
