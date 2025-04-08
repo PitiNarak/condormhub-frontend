31712028ea546e1b93c57b263d22a7be92d6b9de
TokiDOkiiiiiiii <napatphong46@gmail.com>
2025-04-08 21:22:42 +0700

    feat: get param and fetch data on page

--- src/app/profile/[lesseeID]/page.tsx
+++ src/app/profile/[lesseeID]/page.tsx
@@ -1,11 +1,54 @@
+import { getProfileByID } from '@/actions/profile/getProfileByID';
 import { LesseeReview } from '@/components/profileLesseeID-page/lesseeReviews';
-import { ProfileSection } from '@/components/profileLesseeID-page/profileSection';
-export default async function page() {
+import { ProfileHeader } from '@/components/profileLesseeID-page/profileHeader';
+import { ProfileInfo } from '@/components/profileLesseeID-page/profileInfo';
+import { redirect } from 'next/navigation';
+
+export default async function page({
+  params,
+}: {
+  params: Promise<{ lesseeID: string }>;
+}) {
+  const lesseeID = (await params).lesseeID;
+  const data = await getProfileByID(lesseeID);
+  if ('message' in data) {
+    redirect('/');
+  }
   return (
     <div className="">
-      <ProfileSection />
       <div className="flex justify-center mx-auto">
-        <LesseeReview />
+        <div className="flex flex-col gap-2 lg:flex-row">
+          <div className="flex justify-center mx-auto w-[350px] md:w-[700px] lg:w-[350px]">
+            <ProfileHeader
+              userName={data.username ?? ''}
+              profileURL={
+                data.profilePicUrl
+                  ? data.profilePicUrl
+                  : 'https://placehold.co/100'
+              }
+              role={data.role ?? ''}
+              ratingScore={5}
+              reviewsAmount={5}
+              totalRenting={5}
+            />
+          </div>
+          <ProfileInfo
+            birthDate={data.birthDate ?? ''}
+            createAt={data.createAt ?? ''}
+            email={data.email ?? ''}
+            firstName={data.firstname ?? ''}
+            lastName={data.lastname ?? ''}
+            userName={data.username ?? ''}
+            gender={data.gender ?? ''}
+            studentAt={data.gender ?? ''}
+            life1={data.lifestyles ? (data.lifestyles[0] ?? '') : 'Error'}
+            life2={data.lifestyles ? (data.lifestyles[1] ?? '') : 'Error'}
+            life3={data.lifestyles ? (data.lifestyles[2] ?? '') : 'Error'}
+          />
+        </div>
+      </div>
+      <div className="flex justify-center mx-auto">
+        <LesseeReview lesseeID={lesseeID} />
       </div>
     </div>
   );
