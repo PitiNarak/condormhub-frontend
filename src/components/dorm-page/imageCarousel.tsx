8b7027c7a022a4a8c65a8a0fa0b53bdcaed7029b
Pongpak Manoret <pongpak282546@gmail.com>
2025-04-08 19:48:47 +0700

    feat: add X on the dorm image

--- src/components/dorm-page/imageCarousel.tsx
+++ src/components/dorm-page/imageCarousel.tsx
@@ -10,11 +10,29 @@ import {
 } from '@/components/ui/carousel';
 import Image from 'next/image';
 import React from 'react';
+import { X } from 'lucide-react';
+import { useRouter } from 'next/navigation';
+import { deleteDorm } from '@/actions/editDorm/deleteDorm';
 
 export const ImageCarousel = ({ images }: { images: Array<string> }) => {
   const [api, setApi] = React.useState<CarouselApi>();
   const [current, setCurrent] = React.useState(0);
   const [count, setCount] = React.useState(0);
+  const router = useRouter();
+  async function onSubmit(index: number) {
+    try {
+      const url = images[index];
+      images.splice(index, 1);
+      if (current == count) {
+        setCurrent(current - 1);
+      }
+      setCount(count - 1);
+      deleteDorm(url);
+      router.refresh();
+    } catch (e: unknown) {
+      console.log(e);
+    }
+  }
 
   React.useEffect(() => {
     if (!api) {
@@ -39,7 +57,7 @@ export const ImageCarousel = ({ images }: { images: Array<string> }) => {
         <CarouselContent>
           {images.map((_, index) => (
             <CarouselItem key={index}>
-              <Card>
+              <Card className="relative">
                 <CardContent className="flex aspect-square items-center justify-center p-0">
                   <Image
                     src={images[index]}
@@ -49,6 +67,14 @@ export const ImageCarousel = ({ images }: { images: Array<string> }) => {
                     className="rounded-xl h-[400px] object-cover"
                   />
                 </CardContent>
+                <div>
+                  <button
+                    className="w-[25px] h-[25px] absolute top-2 right-2 z-10"
+                    onClick={() => onSubmit(index)}
+                  >
+                    <X className="text-gray-300 hover:text-gray-500 transition-colors" />
+                  </button>
+                </div>
               </Card>
             </CarouselItem>
           ))}
