f26cd7d973729b2e70bbfd06845a3c8251c33d7e
TokiDOkiiiiiiii <napatphong46@gmail.com>
2025-04-08 21:17:12 +0700

    fix: add authorization token to getProfileByID

--- src/actions/profile/getProfileByID.ts
+++ src/actions/profile/getProfileByID.ts
@@ -1,15 +1,21 @@
 'use server';
 
 import client from '@/api';
+import { auth } from '@/lib/auth';
 
 export async function getProfileByID(lesseeID: string) {
+  const session = await auth();
   const { data, error } = await client.GET('/user/{id}', {
     params: {
       path: {
         id: lesseeID,
       },
     },
+    headers: {
+      Authorization: `Bearer ${session?.access_token}`,
+    },
   });
+  console.log(data)
   if (error || !data.data) {
     return {
       message: error?.error,
