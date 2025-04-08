56136294f64edb538e0e891736886df2b27814f4
TokiDOkiiiiiiii <napatphong46@gmail.com>
2025-04-08 23:25:12 +0700

    feat: let page handle searchParams

--- src/app/rentHistory/page.tsx
+++ src/app/rentHistory/page.tsx
@@ -1,7 +1,17 @@
-export default function Page() {
+import { HistoryList } from '@/components/rentHistory-page/historyList';
+
+export default async function Page({
+  searchParams,
+}: {
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+}) {
+  const page = await searchParams;
   return (
     <div>
-      <p>Leasing History</p>
+      <p className="text-center text-3xl font-bold">Leasing History</p>
+      <div>
+        <HistoryList page={Number(page)} />
+      </div>
     </div>
   );
 }
