9b1da8b3a8acf6482e02f5e7c150a68a3e8219a1
Naron <naron834@gmail.com>
2025-04-09 16:25:27 +0700

    refactor: move title to center

--- src/app/requestHistory/page.tsx
+++ src/app/requestHistory/page.tsx
@@ -1,12 +1,12 @@
 import { RequestScroll } from '@/components/leasingRequest-page/requestScroll';
-import { Divider } from '@/components/navigationBar/divider';
 
 const page = () => {
   return (
     <div className="flex flex-col justify-center items-center p-10 gap-6">
       <div className="flex flex-col gap-3 max-w-3xl w-full">
-        <h1 className="text-3xl pt-3 font-semibold">Request History</h1>
-        <Divider className="max-w-3xl w-full" />
+        <h1 className="text-3xl pt-3 font-semibold text-center">
+          Request History
+        </h1>
         <RequestScroll isLessor={false} />
       </div>
     </div>
