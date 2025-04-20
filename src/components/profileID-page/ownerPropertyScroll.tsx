8ca0e4479d5ca0556174ce39423286d1c7f99498
TokiDOkiiiiiiii <napatphong46@gmail.com>
2025-04-20 23:50:23 +0700

    fix: add s to [lessor name]'s dorms

--- src/components/profileID-page/ownerPropertyScroll.tsx
+++ src/components/profileID-page/ownerPropertyScroll.tsx
@@ -90,7 +90,7 @@ export async function OwnerPropertyScroll({
         <div className="flex flex-col justify-center items-center p-10 gap-6">
           <div className="flex flex-col gap-3 max-w-3xl w-full">
             <h1 className="text-3xl pt-3 font-semibold text-center">
-              {ownerName}&apos; dorms
+              {ownerName}&apos;s dorms
             </h1>
           </div>
         </div>
