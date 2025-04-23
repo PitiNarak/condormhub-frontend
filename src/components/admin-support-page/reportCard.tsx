b8a6b84b8ffb126d9d719b41d135c77d71a0b801
Pongpak Manoret <pongpak282546@gmail.com>
2025-04-24 02:24:35 +0700

    feat: fetch username

--- src/components/admin-support-page/reportCard.tsx
+++ src/components/admin-support-page/reportCard.tsx
@@ -1,3 +1,6 @@
+import { getProfileByID } from '@/actions/profile/getProfileByID';
+import { useEffect, useState } from 'react';
+
 interface ReportCardProp {
   reporter: string;
   date: string;
@@ -11,15 +14,31 @@ export function ReportCard({
   message,
   status,
 }: ReportCardProp) {
+  const [displayName, setDisplayName] = useState('');
+
+  useEffect(() => {
+    const fetchProfile = async () => {
+      try {
+        const result = await getProfileByID(reporter);
+        if ('firstname' in result) {
+          setDisplayName(result.firstname ?? '');
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchProfile();
+  }, [reporter]);
+
   const displayDate =
     date === '0001-01-01T00:00:00Z'
       ? ''
       : new Date(date).toUTCString().slice(4, 16);
-
   return (
     <div className="flex gap-4 p-4 border rounded-lg shadow-md items-start bg-gray-50 w-full">
       <span>{displayDate}</span>
-      <span>{reporter}</span>
+      <span>{displayName}</span>
       <span>{message}</span>
       <span>{status}</span>
     </div>
