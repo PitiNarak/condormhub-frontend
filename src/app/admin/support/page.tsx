8fb0c1fff6d24ea769e1310e93dd08308dd32e0f
Pongpak Manoret <pongpak282546@gmail.com>
2025-04-24 03:54:41 +0700

    refactor: change py value

--- src/app/admin/support/page.tsx
+++ src/app/admin/support/page.tsx
@@ -2,7 +2,7 @@ import { ReportScroll } from '@/components/admin-support-page/reportScroll';
 
 export default function Page() {
   return (
-    <div className="flex justify-center items-center py-10 px-4">
+    <div className="flex justify-center items-center  px-4">
       <ReportScroll />
     </div>
   );
