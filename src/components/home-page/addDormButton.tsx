e9fe3bd8daec8045e438ba448afff33e07e59508
Pongpak Manoret <pongpak282546@gmail.com>
2025-04-23 20:57:18 +0700

    feat: change desing

--- src/components/home-page/addDormButton.tsx
+++ src/components/home-page/addDormButton.tsx
@@ -6,11 +6,14 @@ import { useRouter } from 'next/navigation';
 export const AddDormButton = () => {
   const router = useRouter();
   return (
-    <div className="fixed bottom-2 right-5 border-4 rounded-full p-1 mb-2 border-gray-200 cursor-pointer group hover:border-gray-400 transition-colors duration-200 ">
-      <Plus
-        className="text-gray-200 h-12 w-12 group-hover:text-gray-400 transition-colors duration-200"
+    <div className="fixed  bottom-3 right-5 w-44 bg-gray-100 border-4 rounded-xl p-1 mb-2 border-black cursor-pointer hover:bg-gray-200 transition-colors duration-200">
+      <div
+        className="flex justify-center align-middle "
         onClick={() => router.push('/dorm/register')}
-      />
+      >
+        <Plus className="text-black h-8 w-8" />
+        <span className="pt-1 pl-1">Add dorminatory</span>
+      </div>
     </div>
   );
 };
