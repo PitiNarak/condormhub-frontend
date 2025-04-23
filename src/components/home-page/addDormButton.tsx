7c5e3f89abe48ec6a2011f9b3455fc9b4250ca4b
Pongpak Manoret <pongpak282546@gmail.com>
2025-04-23 22:59:25 +0700

    refactor: change button size

--- src/components/home-page/addDormButton.tsx
+++ src/components/home-page/addDormButton.tsx
@@ -6,7 +6,7 @@ import { useRouter } from 'next/navigation';
 export const AddDormButton = () => {
   const router = useRouter();
   return (
-    <div className="fixed  bottom-3 right-5 w-44 bg-gray-950 border-gray-950 border-4 rounded-xl p-1 mb-2 cursor-pointer hover:bg-gray-800 transition-colors duration-200 hover:border-gray-800">
+    <div className="fixed  bottom-3 right-5 w-40 bg-gray-950 border-gray-950 border-4 rounded-xl p-1 mb-2 cursor-pointer hover:bg-gray-800 transition-colors duration-200 hover:border-gray-800">
       <div
         className="flex justify-center align-middle "
         onClick={() => router.push('/dorm/register')}
